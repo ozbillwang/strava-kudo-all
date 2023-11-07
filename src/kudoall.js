dde83916c54282c4ac08cbe641eccf56bebdd68a
Bill Wang <bill.wang>
2023-11-07 22:41:28 +1100

    bug fix

--- src/kudoall.js
+++ src/kudoall.js
@@ -1,29 +1,23 @@
+// Internationalization support
 let _intl;
 
 try {
-    if (window.chrome !== undefined) {
-        _intl = chrome.i18n;
-    } else {
-        _intl = browser.i18n;
-    }
+    _intl = window.chrome !== undefined ? chrome.i18n : browser.i18n;
 } catch (err) {
     _intl = {
         getMessage: function (messageName, substitutions) {
-            if (substitutions) {
-                return substitutions;
-            }
-
-            return messageName;
+            return substitutions || messageName;
         }
-    }
+    };
 }
 
 function getMessage(messageName, substitutions) {
     return _intl.getMessage(messageName, substitutions);
 }
 
+// Retrieve or create the container for the Kudo All button
 function getContainer() {
-    let container =  document.querySelector(".feed-header");
+    let container = document.querySelector(".feed-header");
 
     if (!container) {
         container = document.querySelector(".feed-ui").parentElement.querySelector('form');
@@ -32,117 +26,89 @@ function getContainer() {
 
         const el = document.createElement("div");
         el.classList.add("feed-header");
-        el.style.height = "40px";
+        el.style.cssText = "height: 40px; display: flex; justify-content: end;";
         container.append(el);
-        el.style.display = "flex";
-        el.style.justifyContent = "end";
     } else {
-        container.style.display = "flex";
-        container.style.justifyContent = "space-between";
+        container.style.cssText = "display: flex; justify-content: space-between;";
     }
 
     return document.querySelector(".feed-header");
 }
 
+// Find all Kudos buttons within the specified container
 function findKudosButtons(container) {
     const selector = "button[data-testid='kudos_button'] > svg[data-testid='unfilled_kudos']";
-
-    if (!container) {
-        return Array.from(document.querySelectorAll(selector));
-    }
-
-    return Array.from(container.querySelectorAll(selector));
+    return Array.from((container || document).querySelectorAll(selector));
 }
 
+// Create a filter to exclude activities from the current athlete
 function createFilter(athleteLink) {
-    const href = athleteLink.href
-        .replace("https://www.strava.com", "")
-        .replace("https://strava.com", "");
-
+    const href = athleteLink.href.replace(/https:\/\/(www\.)?strava\.com/, "");
     return item => !item.querySelector(`a[href^="${href}"]`);
 }
 
+// Retrieve all Kudos buttons, excluding those for the current athlete's activities
 function getKudosButtons() {
     const athleteLink = document.querySelector("#athlete-profile a[href^='/athletes']");
-
     if (!athleteLink) {
         return findKudosButtons();
     }
 
     let activities = document.querySelectorAll("div[data-testid='web-feed-entry']");
-
-    if (activities.length < 1) {
+    if (activities.length === 0) {
         return findKudosButtons();
     }
 
     activities = Array.from(activities).filter(createFilter(athleteLink));
-
-    if (activities.length < 1) {
-        return findKudosButtons();
-    }
-
-    return activities.flatMap(findKudosButtons).filter(item => !!item);
+    return activities.length > 0 ? activities.flatMap(findKudosButtons).filter(Boolean) : [];
 }
 
+// Create the Kudo All button element
 function createButton() {
     const label = getMessage("kudo_all", "Kudo All");
-
     const navItem = document.createElement("div");
     navItem.style.display = "flex";
 
     const style = `
-    margin-top: 0;
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    max-width: 200px;
-    float: right;
-`;
+        margin-top: 0;
+        padding: 0.75rem 0;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        max-width: 200px;
+        float: right;
+    `;
 
     navItem.innerHTML = `
-    <button type="button" class="btn btn-default btn-sm empty" style="${style}">
-        <div class="app-icon icon-kudo" style="margin-right: 10px;">${label}</div>
-        <div class="ka-progress text-caption1">${label}</div>
-    </button>
+        <button type="button" class="btn btn-default btn-sm empty" style="${style}">
+            <div class="app-icon icon-kudo" style="margin-right: 10px;">${label}</div>
+            <div class="ka-progress text-caption1">${label}</div>
+        </button>
     `;
 
     return navItem;
 }
 
+// Handle the Kudo All button click event
 function kudoAllHandler(event) {
     event.preventDefault();
 
     const icons = getKudosButtons();
-    const len = icons.length;
-
-    if (len < 1) {
-        return;
-    }
-
-    for (let i = 0; i < len; i++) {
-        const item = icons[i];
-
-        if (!item) {
-            continue;
+    icons.forEach(icon => {
+        const parentButton = icon.parentElement;
+        if (parentButton) {
+            parentButton.click();
         }
-
-        const parentItem = item.parentElement;
-
-        if (parentItem) {
-            parentItem.click();
-        }
-    }
+    });
 }
 
+// Initialize the script after the window loads
 window.onload = function () {
     const container = getContainer();
-
     if (container) {
         const button = createButton();
         container.append(button);
-
         button.addEventListener("click", kudoAllHandler);
     }
-}
+};
+
